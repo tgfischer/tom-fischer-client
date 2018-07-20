748c82ba7297a9b439a4e3079e1961e7c4129e03
Tom Fischer <tgfischer6@gmail.com>
2018-07-19 23:38:58 -0400

    Updated serverless-build-client

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,5 +4,5 @@ const minimist = require("minimist");
 const { stage = "prod" } = minimist(process.argv.slice(2));
 
 shell.exec(`npx serverless deploy --stage ${stage} --verbose`);
-shell.exec("npx serverless build");
+shell.exec("npx serverless client build");
 shell.exec("npx serverless client deploy --no-confirm");
